db7c75a8e3aa3bc0ad6d0686f0b1b5c27872527a
LKRWN <lakruwaninguruwatte002@gmail.com>
2025-05-19 13:52:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll to Features section
-document.getElementById('scrollToFeatures').onclick = function() {
-  document.getElementById('features').scrollIntoView({ behavior: 'smooth' });
-};
-
-// Back to top (landing)
-document.getElementById('backToLanding').onclick = function() {
-  document.getElementById('landing').scrollIntoView({ behavior: 'smooth' });
-};
